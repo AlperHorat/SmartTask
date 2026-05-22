e4b4b58f1e763246021bb62d079b50c065db86aa
AlperHorat <alperhorat@gmail.com>
2026-05-22 13:05:53 +0300

    edited configs

---
+++ src/SmartTask.Application/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace SmartTask.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // MediatR
+            services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+            });
+
+            // FluentValidation
+            //services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            // Pipeline Behaviors
+            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            return services;
+        }
+    }
+}
